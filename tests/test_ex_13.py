f7b41ab34ba346c548fabcbf84bc64e4e4af4af1
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-11-12 14:09:47 +0300

    [x] Задание 19. Реализовать многослойную архитектуру

--- tests/test_ex_13.py
+++ tests/test_ex_13.py
@@ -1,8 +1,6 @@
 
 def test_items_in_cart(app):
-    n = 3
-    quantity = 1
     product_list = app.cart.get_product_list()
-    for ind in range(n):
-        app.cart.add_item_to_cart(product_list, quantity)
+    for ind in range(3):
+        app.cart.add_item_to_cart(product_list, 1)
     app.cart.del_items_from_cart()
